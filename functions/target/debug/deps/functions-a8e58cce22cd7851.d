f21caba7ec6a1e12d844113cc9c1c7b4c56b72b0
Davidm8624 <davidm8624@gmail.com>
2023-12-11 16:48:13 -0700

    rgfasrgrsg

---
+++ functions/target/debug/deps/functions-a8e58cce22cd7851.d
@@ -0,0 +1,5 @@
+/home/david/Desktop/Code/rust-practice/functions/target/debug/deps/functions-a8e58cce22cd7851: src/main.rs
+
+/home/david/Desktop/Code/rust-practice/functions/target/debug/deps/functions-a8e58cce22cd7851.d: src/main.rs
+
+src/main.rs:
---
+++ functions/target/debug/functions.d
@@ -0,0 +1,1 @@
+/home/david/Desktop/Code/rust-practice/functions/target/debug/functions: /home/david/Desktop/Code/rust-practice/functions/src/main.rs
