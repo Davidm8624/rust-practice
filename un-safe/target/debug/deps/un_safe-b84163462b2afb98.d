f00dc11d1ff964d5a77487f65349c54113074070
Davidm8624 <davidm8624@gmail.com>
2023-12-16 11:08:08 -0700

    todayas

---
+++ un-safe/target/debug/deps/un_safe-b84163462b2afb98.d
@@ -0,0 +1,5 @@
+/home/david/Desktop/Code/rust-practice/un-safe/target/debug/deps/un_safe-b84163462b2afb98: src/main.rs
+
+/home/david/Desktop/Code/rust-practice/un-safe/target/debug/deps/un_safe-b84163462b2afb98.d: src/main.rs
+
+src/main.rs:
---
+++ un-safe/target/debug/un-safe.d
@@ -0,0 +1,1 @@
+/home/david/Desktop/Code/rust-practice/un-safe/target/debug/un-safe: /home/david/Desktop/Code/rust-practice/un-safe/src/main.rs
